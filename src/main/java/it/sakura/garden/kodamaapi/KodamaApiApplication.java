759f66f8527caddacdcf5d2df4078c85cc3061e9
alexddev <alessandrocostantino.info@gmail.com>
2026-08-16 17:37:03 +0200

    Setup

---
+++ src/main/java/it/sakura/garden/kodamaapi/KodamaApiApplication.java
@@ -0,0 +1,13 @@
+package it.sakura.garden.kodamaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodamaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodamaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/sakura/garden/kodamaapi/KodamaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sakura.garden.kodamaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodamaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
