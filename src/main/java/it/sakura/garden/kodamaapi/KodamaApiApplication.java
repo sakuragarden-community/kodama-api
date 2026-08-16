a5db057b897dd8b04f1046050d78721454f68db6
alexddev <alessandrocostantino.info@gmail.com>
2026-08-16 17:37:22 +0200

    Setup

--- src/main/java/it/sakura/garden/kodamaapi/KodamaApiApplication.java
+++ src/main/java/it/sakura/garden/kodamaapi/KodamaApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KodamaApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KodamaApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KodamaApiApplication.class, args);
+    }
 
 }
